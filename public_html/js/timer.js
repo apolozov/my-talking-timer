becc7a93622df748f6c0735e7294317d089ebd50
apolozov <>
2014-01-31 22:24:13 -0500

    Very basic skeleton

---
+++ public_html/js/timer.js
@@ -0,0 +1,27 @@
+var model = (function() 
+{
+    var running = false;
+    var interval = 0;
+    var targetTime;
+    
+    var getRemainingSeconds = function()
+    {
+    };
+    
+    var start = function() 
+    {
+        
+    };
+    
+    var stop = function() 
+    {
+    };
+    
+    var everySecond = function() 
+    {
+    };
+    var render = function() 
+    {
+    };
+})();
+
