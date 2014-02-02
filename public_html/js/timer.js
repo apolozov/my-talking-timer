176675e2b2276e56e0bc9e0e14aeb7ff305b6154
apolozov <>
2014-02-01 23:06:31 -0500

    Learning how to hook event handlers in jquery

--- public_html/js/timer.js
+++ public_html/js/timer.js
@@ -1,27 +1,59 @@
-var model = (function() 
+var timer = (function() 
 {
     var running = false;
-    var interval = 0;
+    var inputTime = 0;
     var targetTime;
-    
-    var getRemainingSeconds = function()
-    {
-    };
     
     var start = function() 
     {
-        
+        console.log("Starting");
+        running = true;
+        inputTime = getInputTime();
+        var now = (new Date()).getTime();
+        targetTime = now + (inputTime * 1000);
+        render();
+    };
+    
+    var getInputTime = function()
+    {
+        var timeField = $("#time-input");
+        var timeString = timeField.val();
+        return timeString * 1;
+    };
+    
+    var getTimeLeft = function()
+    {
+        var now = new Date();
+        return (targetTime - now.getTime());
     };
     
     var stop = function() 
     {
+        running = false;
     };
     
-    var everySecond = function() 
+    var tick = function() 
     {
+        render();
     };
+    
     var render = function() 
     {
+        var sleft = getTimeLeft();
+        $("#seconds-left").text(sleft);
+    };
+    
+    var init = function()
+    {
+        console.log("Initializing");
+        $("#tick").click(tick);
+        $("#time-input").change(start);
+    };
+    
+     // public API
+    return {
+        init: init
     };
 })();
 
+$( document ).ready( timer.init );
